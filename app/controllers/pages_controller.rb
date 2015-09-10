0150cd1db713fdd037a7a8e12212d8230b70b448
danReynolds <fnetma@hotmail.com>
2015-09-09 23:44:02 -0400

    projects page links setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,27 @@ class PagesController < ApplicationController
   end
 
   def projects
+    @projects = [
+      {
+        logo: 'projects/supermarkit.png',
+        name: 'SupermarKit',
+        summary: 'A community-driven grocery planning and tracking web application.'
+      },
+      {
+        logo: 'projects/rust.svg',
+        name: 'Splash',
+        summary: 'A shell written in the Rust systems programming language.'
+      },
+      {
+        logo: 'projects/boltmade.png',
+        name: 'Boltmade',
+        summary: 'My highlights working on projects and making software at Boltmade.'
+      },
+      {
+        logo: 'projects/raspberry.jpg',
+        name: 'Media player',
+        summary: 'My ongoing mission to make my house the computer from Star Trek.'
+      }
+    ]
   end
 end
