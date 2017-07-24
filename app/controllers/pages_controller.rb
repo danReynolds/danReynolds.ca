4d1224e0259c364e6d3a6af9e386b96d53e63d43
Dan Reynolds <me@danreynolds.ca>
2017-07-23 21:02:59 -0700

    divided projects and works into separate pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,31 +2,47 @@ class PagesController < ApplicationController
   def home
   end
 
-  def projects
-    @projects = [
-      {
-        logo: 'projects/supermarkit.png',
-        name: 'SupermarKit',
-        summary: 'Creator of the grocery planning and tracking web application.',
-        timeline: '2014 - present'
-      },
+  def works
+    @works = [
       {
         logo: 'projects/nerdwallet.png',
         name: 'Nerdwallet',
         summary: 'Software engineer for mobile and web applications.',
-        timeline: '2014 - present'
+        timeline: '2014 - present',
+        link: 'https://nerdwallet.com'
       },
       {
         logo: 'projects/zazzle.jpg',
         name: 'Zazzle',
         summary: 'Software engineer working on Zazzle web products.',
-        timeline: 'Jan - May, 2016'
+        timeline: 'Jan - May, 2016',
+        link: 'https://zazzle.com'
       },
       {
         logo: 'projects/boltmade.png',
         name: 'Boltmade',
         summary: 'Web developer at a software and design consultant firm.',
-        timeline: '2013 - 2015'
+        timeline: '2013 - 2015',
+        link: 'https://boltmade.com'
+      }
+    ]
+  end
+
+  def projects
+    @projects = [
+      {
+        logo: 'projects/summonerexpert.jpg',
+        name: 'Summoner Expert',
+        summary: 'AI Assistant for information on League of Legends.',
+        timeline: '2017 - present',
+        link: 'https://summonerexpert.com'
+      },
+      {
+        logo: 'projects/supermarkit.png',
+        name: 'SupermarKit',
+        summary: 'Side project for tracking and managing groceries with groups.',
+        timeline: '2016 - present',
+        link: 'https://github.com/danReynolds/SupermarKit'
       }
     ]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get :home, to: 'pages#home'
   get :projects, to: 'pages#projects'
+  get :works, to: 'pages#works'
 
   root 'pages#home'
 end
